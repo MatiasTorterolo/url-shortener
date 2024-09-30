8f2f0fb0aa9d381cdbcdee7dccbcf91bd931d6d2
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-09-30 17:55:21 -0300

    urlController development

--- src/main/java/com/url/shortener/url_shortener/controllers/UrlController.java
+++ src/main/java/com/url/shortener/url_shortener/controllers/UrlController.java
@@ -1,16 +1,52 @@
 package com.url.shortener.url_shortener.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.util.UUID;
 
 @Controller
-@RequestMapping("/url")
 public class UrlController {
 
-    @GetMapping("/shorten")
-    public String shortenUrl() {
+    @PostMapping("/shorten")
+    public String shortenUrl(@RequestParam("url") String url, Model model) {
 
-        return "urlShortenerView";
+        if (isValidUrl(url)) {
+
+            String urlShortened = shortUrl(url);
+
+            model.addAttribute("url", url);
+            model.addAttribute("showUrlShortened", urlShortened);
+
+            return "homeView";
+
+        } else {
+
+            model.addAttribute("showUrlShortened", isValidUrl(url));
+            return "homeView";
+        }
+    }
+
+    public boolean isValidUrl(String url) {
+
+        try {
+            new URL(url);
+            return true;
+        } catch (MalformedURLException e) {
+
+            return false;
+        }
+    }
+
+    public String shortUrl(String url) {
+
+        url = UUID.randomUUID().toString().replace("-", "").substring(0, 5);
+
+        return url;
     }
 }
