cb9f7136802885d726eabdfaf10fd2cf9378c138
MatiasTorterolo <mtorterolo.a@gmail.com>
2024-10-03 18:03:40 -0300

    added the documentation

--- src/main/java/com/url/shortener/url_shortener/controllers/HomeController.java
+++ src/main/java/com/url/shortener/url_shortener/controllers/HomeController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Home Controller
+ * 
+ * @author MTA
+ */
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
+    /**
+     * This method handles http GET requests in some rutes like "/", "", "/home".
+     * 
+     * @return The string with view name.
+     */
     @GetMapping({ "", "/", "/home" })
     public String home() {
 
--- src/main/java/com/url/shortener/url_shortener/services/UrlService.java
+++ src/main/java/com/url/shortener/url_shortener/services/UrlService.java
@@ -9,17 +9,36 @@ import org.springframework.stereotype.Service;
 import com.url.shortener.url_shortener.entities.UrlEntity;
 import com.url.shortener.url_shortener.repositories.IUrlRepository;
 
+/**
+ * This service handles the logic to create and find url entities, it also check
+ * if url exist or not in the data base.
+ * 
+ * @author MTA
+ */
 @Service
 public class UrlService {
 
     @Autowired
     private IUrlRepository iUrlRepository;
 
+    /**
+     * This method create a new url entity.
+     * 
+     * @param entity A url entity.
+     * @return The saved url entity.
+     */
     public UrlEntity createUrlEntity(UrlEntity entity) {
 
         return iUrlRepository.save(entity);
     }
 
+    /**
+     * This method creates a short url from the long url, if the long url exists the
+     * saved short url will be shown.
+     * 
+     * @param longUrl A long url.
+     * @return The short url linked to the long url.
+     */
     public String generateShortUrl(String longUrl) {
 
         Optional<UrlEntity> entity = iUrlRepository.findByLongUrl(longUrl);
@@ -40,6 +59,12 @@ public class UrlService {
 
     }
 
+    /**
+     * This method searches the url on the data base.
+     * 
+     * @param shortUrl The short url.
+     * @return An optional url entity empty or present.
+     */
     public Optional<UrlEntity> findByShortUrl(String shortUrl) {
 
         return iUrlRepository.findByUrlShortener(shortUrl);
